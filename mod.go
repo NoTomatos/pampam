e4add8ced26f9c9142970aa042a34f49f800e6df
NoTomatos <avolerog0142@gmail.com>
2025-12-25 17:08:31 +0300

    changed mod.go

--- mod.go
+++ mod.go
@@ -1,1 +1,21 @@
+package main
 
+type Status string
+
+const (
+	StatusNew        Status = "Новая задача"
+	StatusInProgress Status = "Задача в процессе"
+	StatusCompleted  Status = "Задача выполнена!"
+)
+
+type Task struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      Status `json:"status"`
+}
+
+type TaskStore struct {
+	tasks     []Task
+	idCounter int
+}
